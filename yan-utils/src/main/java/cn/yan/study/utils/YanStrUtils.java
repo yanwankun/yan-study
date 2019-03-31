d345b10ec125bc20f782501f336ec8eb2fb02451
yanwankunde@qq.com <yanwankunde@qq.com>
2019-04-01 00:25:47 +0800

    code add

--- yan-utils/src/main/java/cn/yan/study/utils/YanStrUtils.java
+++ yan-utils/src/main/java/cn/yan/study/utils/YanStrUtils.java
@@ -27,12 +27,56 @@ public class YanStrUtils {
         }
     }
 
+    public static byte[] hexStringToByte(String hex) {
+        int len = (hex.length() / 2);
+        byte[] result = new byte[len];
+        char[] achar = hex.toCharArray();
+        for (int i = 0; i < len; i++) {
+            int pos = i * 2;
+            result[i] = (byte) (toByte(achar[pos]) << 4 | toByte(achar[pos + 1]));
+        }
+        return result;
+    }
+
+    private static byte toByte(char c) {
+        byte b = (byte) "0123456789abcdef".indexOf(c);
+        return b;
+    }
+
+
     public static String getYanString(String prefix) {
         StringBuffer sb = new StringBuffer();
         sb.append(prefix);
         sb.append(YanDateUtils.getDateString(YanDateUtils.TIME_STRING_3));
         sb.append(YanStrUtils.getRandomCode(6));
         return sb.toString();
+    }
+
+    public static String hexString2binaryString(String hexString) {
+        if (hexString == null || hexString.length() % 2 != 0)
+            return null;
+        String bString = "", tmp;
+        for (int i = 0; i < hexString.length(); i++) {
+            tmp = "0000" + Integer.toBinaryString(Integer.parseInt(hexString.substring(i, i + 1), 16));
+            bString += tmp.substring(tmp.length() - 4);
+        }
+        return bString;
+    }
+
+
+    public static String binaryString2hexString(String bString) {
+        if (bString == null || bString.equals("") || bString.length() % 8 != 0)
+            return null;
+        StringBuffer tmp=new StringBuffer();
+        int iTmp = 0;
+        for (int i = 0; i < bString.length(); i += 4) {
+            iTmp = 0;
+            for (int j = 0; j < 4; j++) {
+                iTmp += Integer.parseInt(bString.substring(i + j, i + j + 1)) << (4 - j - 1);
+            }
+            tmp.append(Integer.toHexString(iTmp));
+        }
+        return tmp.toString();
     }
 
     // todo
